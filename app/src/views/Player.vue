2802c8640858aef324c5f709e36b2e9b84cfa7ba
Erestine Bytyqi <erestinabytyqi49282@gmail.com>
2022-07-22 22:11:27 +0200

    About Us

--- app/src/views/Player.vue
+++ app/src/views/Player.vue
@@ -1,21 +1,17 @@
 <template>
-    <div class="player">
-       <div class="player_sizer">
-           <video controls>
-              <source type="video/mp4" src="../assets/borabora.mp4" />
-           </video>
-       </div>
-    </div>
+   <div class="player">
+      <div class="player_sizer">
+         <video controls>
+            <source type="video/mp4" src="../assets/borabora.mp4" />
+         </video>
+      </div>
+   </div>
 </template>
 <script>
-import Player from "../components/Player";
 import "../components/Player/Player.scss";
 
 export default {
    name: 'Player',
 
-   components: {
-    Player
-  }
 }
 </script>
