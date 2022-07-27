80eceb99645ccff9c8dc4e98d19335f901b5c582
Edisona <rexhaedisona@gmail.com>
2022-07-27 17:19:49 +0200

    Player changes

--- app/src/views/Player.vue
+++ app/src/views/Player.vue
@@ -13,12 +13,5 @@ import "../components/Player/Player.scss";
 export default {
   name: "Player",
 
-<<<<<<< HEAD
 }
-=======
-  components: {
-   //  Player,
-  },
-};
->>>>>>> 034e75f37b590ab07f61a8e000f75f7ace570576
 </script>
